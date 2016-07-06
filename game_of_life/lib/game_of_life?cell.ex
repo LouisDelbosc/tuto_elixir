750633f2fa7e818a45cc7d49768fb36f67be6118
Louis Delbosc <louis@sqreen.io>
2016-07-06 18:28:43 +0200

    begining of game of life and test

---
+++ game_of_life/lib/game_of_life?cell.ex
@@ -0,0 +1,31 @@
+defmodule GameOfLife.Cell do
+  def keep_alive?(alive_cells, {x, y} = _alive_cell) do
+    case count_neighbours(alive_cells,{x, y}) do
+      2 -> true
+      3 -> true
+      _ -> false
+    end
+  end
+
+  def become_alive?(alive_cells, {x, y} = _dead_cell) do
+    3 == count_neighbours(alive_cells, {x, y})
+  end
+
+  defp count_neighbours(grid, {x, y}) do
+    offsets = [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+               {0, 1}, {1, -1}, {1, 0}, {1, 1}]
+    Enum.reduce(offsets, 0, fn({x_off, y_off}, acc) ->
+      acc + if Set.member?(grid, {x + x_off, y + y_off}), do: 1, else: 0
+    end)
+  end
+
+  def dead_neighbours(alive_cells) do
+    neighbours = neighbours(alive_cells, [])
+    MapSet.difference(neighbours, alive_cells)
+  end
+
+  defp neighbours(stuff, array) do
+    array
+  end
+
+end
